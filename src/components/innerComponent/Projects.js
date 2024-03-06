9b1b2f6b193f715c39852b80883726cf10e48722
mahendra <solankirakesh895@gmail.com>
2024-03-06 23:57:40 +0530

    update:update unwanted minor styling

--- src/components/innerComponent/Projects.js
+++ src/components/innerComponent/Projects.js
@@ -64,7 +64,7 @@ const Projects = () => {
              <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
              <svg width="64px" height="64px" viewBox="-4.8 -4.8 57.60 57.60" xmlns="http://www.w3.org/2000/svg" fill="#f5c724" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.096"></g><g id="SVGRepo_iconCarrier"> <path d="M0 0h48v48H0z" fill="none"></path> <g id="Shopicon"> <circle cx="24" cy="24" r="4"></circle> <path d="M24,38c12,0,20-14,20-14s-8-14-20-14S4,24,4,24S12,38,24,38z M24,16c4.418,0,8,3.582,8,8s-3.582,8-8,8s-8-3.582-8-8 S19.582,16,24,16z"></path> </g> </g></svg>
              </div>
-             <img style={{border:"1px solid green"}} className="project-img" src={project.image} alt={project.name} />
+             <img  style={{boxShadow:"rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;"}} className="project-img" src={project.image} alt={project.name} />
              </div>
              <figcaption className="project-name ">{project.name}</figcaption>
             </figure>
