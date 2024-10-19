ce1f79bb3b5909fa8e6aa874011db7e7d4ebb723
mahendra <solankirakesh895@gmail.com>
2024-10-19 13:34:22 +0530

    updated some needy code

--- src/components/innerComponent/Projects.js
+++ src/components/innerComponent/Projects.js
@@ -85,7 +85,7 @@ const Projects = () => {
  </button>
  <button  onclick="window.location.href='http://google.com';" title="Save" class="save-button">
  <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 8L3 11.6923L7 16M17 8L21 11.6923L17 16M14 4L10 20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-   <a href={selectedProject.demoLink} target="_blank" rel="noopener noreferrer">See code</a>
+   <a href={selectedProject.codeLink} target="_blank" rel="noopener noreferrer">See code</a>
  </button>
    </div>
     </Fragment>
--- src/images/Images.js
+++ src/images/Images.js
@@ -1,6 +1,6 @@
 export const  project1 = require("../images/project1.png");
 export const  project2 = require("../images/project2.png");
-export const  project3 = require("../images/project3.PNG");
+export const  project3 = require("../images/project3.png");
 export const  project4 = require("../images/project4.png");
 
 
